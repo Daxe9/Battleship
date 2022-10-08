18e0269146b1e8b932baa5c4fe5314a65a10dcc5
_xie <davidexie694@gmail.com>
2022-10-08 23:32:31 +0200

    refactored code

--- battleship-frontend/src/components/Board.vue
+++ battleship-frontend/src/components/Board.vue
@@ -1,70 +1,95 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import type {Cell} from "@/types/interfaces";
 import CellComponent from "@/components/CellComponent.vue"
 
+const neighbors: Array<Array<number>> = [
+	[-1, -1],
+	[-1, 0],
+	[-1, 1],
+	[0, -1],
+	[0, 1],
+	[1, -1],
+	[1, 0],
+	[1, 1]
+];
+
 const emit = defineEmits(["submitCoordinates"]);
 const props = defineProps({
-    isDesign: {
-        type: Boolean,
-        required: true
-    },
-    board: {
-        required: true,
-        type: Array<Array<Cell>>
-    },
-    turn: {
-        required: true,
-        type: Boolean
-    }
+	isDesign: {
+		type: Boolean,
+		required: true
+	},
+	board: {
+		required: true,
+		type: Array<Array<Cell>>
+	},
+	turn: {
+		required: true,
+		type: Boolean
+	}
 })
 
+function cellValidation(x: number, y: number): boolean {
+	let isValid: boolean = true;
+	for (let i = 0; i < neighbors.length; ++i) {
+		try {
+			if (props.board[x + neighbors[i][0]][y + neighbors[i][1]].isShip) {
+				isValid = false;
+				break;
+			}
+		} catch (e) {
+		}
+	}
+	return isValid;
+}
+
 function changeCell(x: number, y: number) {
-    if (props.isDesign) {
-        props.board[x][y].isShip = true;
-    } else {
-        if (props.turn) {
-            emit("submitCoordinates", {x, y});
-        }
-    }
+	if (props.isDesign) {
+		props.board[x][y].isShip = true;
+	} else {
+		if (props.turn) {
+			emit("submitCoordinates", {x, y});
+		}
+	}
 }
 
 function checkCellColor(x: number, y: number): string {
-    if (props.board[x][y].visible && props.board[x][y].isShip) {
-        return "red"
-    }
-    if (props.board[x][y].isShip) {
-        return "blue";
-    }
+	if (props.board[x][y].visible && props.board[x][y].isShip) {
+		return "red"
+	}
+	if (props.board[x][y].isShip) {
+		return "blue";
+	}
 
-    if (props.board[x][y].visible) {
-        return "orange"
-    } else {
-        return "green"
-    }
+	if (props.board[x][y].visible) {
+		return "orange"
+	} else {
+		return "green"
+	}
 }
 </script>
 <template>
-    <div class="container">
-        <div v-for="(row, i) in board" :key="i" class="row">
-            <CellComponent
-                v-for="(_, j) in row"
-                :key="j"
-                :color="checkCellColor(i, j)"
-                :id="i + '_' + j"
-                @click="changeCell(i, j)"
-            >
-            </CellComponent>
-        </div>
-    </div>
+	<div class="container">
+		<div v-for="(row, i) in board" :key="i" class="row">
+			<CellComponent
+				v-for="(_, j) in row"
+				:id="i + '_' + j"
+				:key="j"
+				:color="checkCellColor(i, j)"
+				@click="changeCell(i, j)"
+			>
+			</CellComponent>
+		</div>
+	</div>
 </template>
 <style scoped>
 .container {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
 }
 
 .row {
-    display: flex;
+	display: flex;
 }
 </style>
--- battleship-frontend/src/views/HomeView.vue
+++ battleship-frontend/src/views/HomeView.vue
@@ -9,15 +9,6 @@ function connectToServer(): void {
     router.push("/game");
 }
 
-const gridWidth = 10;
-const matrix = ref<Array<Array<Cell>>>([])
-for (let i = 0; i < gridWidth; i++) {
-    const temp: Array<Cell> = [];
-    for (let j = 0; j < gridWidth; j++) {
-        temp.push({visible: false, isShip: false})
-    }
-    matrix.value.push(temp);
-}
 
 </script>
 
